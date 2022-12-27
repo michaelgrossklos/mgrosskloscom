c4f338309c0d4b9771930ed070474c1f27eb9bb7
Michael Großklos <mg@weooo.de>
2022-12-27 11:36:05 +0100

    fix: changed menu link from a link zu router-link

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '@/views/Home.vue'
-import Screencasts from '@/views/Screencasts.vue'
+import Home from './views/Home.vue'
+import Screencasts from './views/Screencasts.vue'
 
 const router = createRouter({
   history: createWebHistory(),
